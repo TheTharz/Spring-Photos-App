ae69c92b0a87f27d07d9fc0de6cb8640dbd5a5e3
Tharindu Imalka <tharinduimalka915@gmail.com>
2023-11-21 08:28:02 +0530

    first commit

---
+++ src/main/java/com/learn/photozapp/PhotozappApplication.java
@@ -0,0 +1,13 @@
+package com.learn.photozapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotozappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotozappApplication.class, args);
+	}
+
+}
